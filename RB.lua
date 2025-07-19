3c2c8a1cd22f1d651a82ffb6d7a5c846657cc663
RBXcod <egor890888@gmail.com>
2025-07-20 02:38:58 +0300

    Update RB.lua

--- RB.lua
+++ RB.lua
@@ -1,3 +1,5 @@
-local library = loadstring(geme:HttpGet("https://github.com/Rob...")
+local library = loadstring(geme:HttpGet("https://github.com/Robojini/Tuturial_UI/blob/main/UI_Template_1")
 
 local window = Library.CreateLib("Name", "RJTheme3")
+
+local tab = window.newTab("player")
